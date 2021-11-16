9730169be1e64f3c30b0fcf3c429658a53410252
aeturrell <a.e.turrell@gmail.com>
2021-11-16 22:18:08 +0000

    monthly pipeline in place

--- grab_datasets.py
+++ grab_datasets.py
@@ -6,6 +6,7 @@ import requests
 from pathlib import Path, PosixPath
 from openpyxl import load_workbook
 import xlrd
+import re
 
 # Read local `config.toml` file.
 config = toml.load("config.toml")
@@ -47,10 +48,14 @@ def find_files(url: str):
 
 def download_and_save_file(file_url: str, file_name: str):
     # Download the file from the top of the list
-    # TODO add check if file exists
-    r = requests.get("https://www.ons.gov.uk" + file_url, stream=True)
-    with open(Path("scratch") / file_name, "wb") as f:
-        f.write(r.content)
+    file_location = Path("scratch") / file_name
+    if file_location.is_file():
+        print("Skipping download; file already exists")
+    else:
+        r = requests.get("https://www.ons.gov.uk" + file_url, stream=True)
+        with open(Path("scratch") / file_name, "wb") as f:
+            f.write(r.content)
+    print("Success: file downloaded")
 
 
 def convert_yyyy_qn_to_datetime(series: pd.Series):
@@ -73,37 +78,41 @@ def find_vintage_from_pub_datetime(df_in: pd.DataFrame):
     return df_in
 
 
-# Quarterly data
-dict_of_urls = config["Q"][0]["urls"]
-dict_of_urls = config["M"][0]["urls"]
-dict_of_files = {k: find_files(v) for k, v in dict_of_urls.items()}
-# restrict to only first file found on each page
-for key, value in dict_of_files.items():
-    dict_of_files[key] = value[0]
+def populate_dataframe_of_data_urls(config, freq):
+    dict_of_urls = config[freq][0]["urls"]
+    dict_of_files = {k: find_files(v) for k, v in dict_of_urls.items()}
+    # restrict to only first file found on each page
+    for key, value in dict_of_files.items():
+        dict_of_files[key] = value[0]
+    # turn this into a dataframe
+    df_urls = pd.DataFrame(dict_of_files, index=["url"]).T
+    df_urls["file_name"] = df_urls["url"].apply(lambda x: x.split("/")[-1])
+    df_urls[["url", "file_name"]].set_index("url").to_dict()
+    # Download the files
+    df_urls.apply(lambda x: download_and_save_file(x["url"], x["file_name"]), axis=1)
+    # add file extensions
+    df_urls["extension"] = df_urls["file_name"].str.split(".").str[1]
+    # Add sheet names
+    df_urls["sheet_names"] = "None"
+    df_urls.loc[df_urls["extension"] == "xlsx", "sheet_names"] = df_urls.loc[
+        df_urls["extension"] == "xlsx", :
+    ].apply(lambda x: get_sheetnames_xlsx(Path("scratch") / x["file_name"]), axis=1)
+    if "xls" in df_urls["extension"].unique():
+        df_urls.loc[df_urls["extension"] == "xls", "sheet_names"] = df_urls.loc[
+            df_urls["extension"] == "xls", :
+        ].apply(lambda x: get_sheetnames_xls(Path("scratch") / x["file_name"]), axis=1)
+    df_urls["sheet_names"] = remove_bad_sheets(df_urls["sheet_names"])
+    df_urls["freq"] = freq
+    return df_urls
 
-# turn this into a dataframe
-df_urls = pd.DataFrame(dict_of_files, index=["url"]).T
-df_urls["file_name"] = df_urls["url"].apply(lambda x: x.split("/")[-1])
 
-
-df_urls[["url", "file_name"]].set_index("url").to_dict()
-
-# Download the files
-df_urls.apply(lambda x: download_and_save_file(x["url"], x["file_name"]), axis=1)
-
-# add file extensions
-df_urls["extension"] = df_urls["file_name"].str.split(".").str[1]
-
-# Add sheet names
-df_urls["sheet_names"] = "None"
-df_urls.loc[df_urls["extension"] == "xlsx", "sheet_names"] = df_urls.loc[
-    df_urls["extension"] == "xlsx", :
-].apply(lambda x: get_sheetnames_xlsx(Path("scratch") / x["file_name"]), axis=1)
-if "xls" in df_urls["extension"].unique():
-    df_urls.loc[df_urls["extension"] == "xls", "sheet_names"] = df_urls.loc[
-        df_urls["extension"] == "xls", :
-    ].apply(lambda x: get_sheetnames_xls(Path("scratch") / x["file_name"]), axis=1)
-df_urls["sheet_names"] = remove_bad_sheets(df_urls["sheet_names"])
+def combined_df_urls(config):
+    df_urls = pd.DataFrame()
+    for freq in ["Q", "M"]:
+        df_urls = pd.concat(
+            [df_urls, populate_dataframe_of_data_urls(config, freq)], axis=0
+        )
+    return df_urls
 
 
 def process_quarterly_file(file_name, sheet_name):
@@ -148,11 +157,43 @@ def process_quarterly_file(file_name, sheet_name):
         units,
         sheet_name,
     )
-    return df.drop("variable", axis=1)
+    # Set up a special case for the unpredictable case of Business Investment
+    if "Total Business Investment" in long_name:
+        _, long_name, measure = re.split(r"(.*?\s.*?\s.*?)\s", long_name)
+        df["code"] = "NPEL"
+        df["long_name"] = long_name
+        df["measure"] = measure
+    df["units"] = df["prices"] + " ; " + df["units"]
+    return df.drop(["variable", "pub_datetime", "estimate", "prices"], axis=1)
 
 
-# df = pd.read_excel(Path("scratch") / df_urls["file_name"].iloc[0], sheet_name="ABJR")
-# df = pd.read_excel(Path("scratch") / df_urls["file_name"].iloc[1], sheet_name="CMVL")
+def process_monthly_file(file_name, sheet_name):
+    df = pd.read_excel(Path("scratch") / file_name, sheet_name=sheet_name)
+    df = df.dropna(how="all", axis=1)
+    df = df.dropna(how="all", axis=0)
+    code = df.columns[0].split(":")[0]
+    long_name = df.columns[0].split("for")[1].strip()
+    measure = df.columns[0].split("for")[1].strip()
+    df.columns = df.loc[4, :]
+    df = df.iloc[1:, :]
+    df = df.rename(
+        columns={"Relating to Period (three months ending)": "Relating to Period"}
+    )
+    # fill in the "latest estimate" entry with a datetime
+    time_series_down = pd.to_datetime(df["Relating to Period"], errors="coerce")
+    time_series_down.iloc[-1] = time_series_down.iloc[-2] + pd.DateOffset(months=1)
+    df["Relating to Period"] = time_series_down
+    df = df[~pd.isna(df["Relating to Period"])]
+    df = pd.melt(df, id_vars="Relating to Period", var_name="datetime")
+    df = df.rename(columns={"Relating to Period": "vintage"})
+    df["long_name"] = long_name
+    df["measure"] = measure
+    df["code"] = code
+    return df
+
+
+df_urls = combined_df_urls(config)
+
 # Testing quarterly pipeline
 file_name = df_urls["file_name"].iloc[2]
 sheet_name = "2011 -"
@@ -161,5 +202,7 @@ df = process_quarterly_file(df_urls["file_name"].iloc[2], "2011 -")
 
 
 # Testing monthly pipeline
-file_name = df_urls["file_name"].iloc[0]
+file_name = df_urls["file_name"].iloc[-2]
 sheet_name = "triangle"
+
+df = process_monthly_file(file_name, sheet_name)
